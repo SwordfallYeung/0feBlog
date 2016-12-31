c01109386b26f0cc7b2c06f9df1d46d6319c809d
yang <2506597416@qq.com>
2016-12-31 18:29:40 +0800

    上传wordpress

---
+++ wordpress/wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<!--<div class="site-info">
+	<a href="<?php /*echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); */?>"><?php /*printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); */?></a>
+</div>--><!-- .site-info -->
+
+<div class="site-info" style="text-align: center;margin-left: 20%;margin-right: 20%">
+	<a href="<?php echo esc_url( __( 'http://yeungworld.0fees.us/wordpress/', 'SWORDFALL' ) ); ?>">Copyright ©2016 Proudly powered by SWORDFALL and WordPress</a>
+</div><!-- .site-info -->
