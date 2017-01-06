51a2cf222f6f3bfeef121b2785cb68e76c8af0c9
yang <2506597416@qq.com>
2017-01-06 16:37:56 +0800

    添加读取https url的文件函数

--- wordpress/wp-admin/includes/noop.php
+++ wordpress/wp-admin/includes/noop.php
@@ -104,3 +104,30 @@ function get_file( $path ) {
 
 	return @file_get_contents( $path );
 }
+
+//杨剑秋 2017.1.6 15:50 修改
+function get_RemoteFile( $path ) {
+
+	$arrContextOptions=array(
+		"ssl"=>array(
+			"verify_peer"=>false,
+			"verify_peer_name"=>false,
+		),
+	);
+	$response = file_get_contents($path, false, stream_context_create($arrContextOptions));
+	return $response;
+
+//	return http_request($fp);
+}
+
+//杨剑秋 2017.1.6 15:50 修改
+function http_request($url){
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,$url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//禁止直接显示获取的内容
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //不验证证书下同
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+	$r = curl_exec($ch); //获取
+	curl_close($ch);
+	return $r;
+}
